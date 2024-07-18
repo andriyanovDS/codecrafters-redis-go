a8e889da2fe046b190dd75d7a84e2d2d1c23a417
dmitriyandriyanov <garro93@gmail.com>
2024-07-18 23:12:52 +0300

    Respond to PING

--- app/server.go
+++ app/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,9 +16,40 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	defer l.Close()
+
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	defer connection.Close()
+
+	reader := bufio.NewReader(connection)
+	var recv string
+	var count int
+
+	for {
+		localRecv, err := reader.ReadString('\n')
+		if err != nil {
+			if err.Error() == "EOF" {
+				fmt.Println("Connection closed by the server")
+			} else {
+				fmt.Printf("Failed to read string with error: %v\n", err)
+			}
+			os.Exit(1)
+		}
+		if len(recv) > 0 {
+			recv += localRecv
+		} else {
+			recv = localRecv
+		}
+		if strings.HasSuffix(recv, "\r\n") {
+			count += 1
+			recv = ""
+		}
+		if count == 3 {
+			connection.Write([]byte("+PONG\r\n"))
+		}
 	}
 }
